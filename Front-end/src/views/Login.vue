9266111338a319ebfff3cb937c5bd09d4bbd1f07
Serena <89974972+Sereentje19@users.noreply.github.com>
2023-09-07 11:21:27 +0200

    login begin

---
+++ Front-end/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <body>
+    <div class="loginContainer">
+      <input class="inputFieldLogin" type="text" placeholder="Email" required />
+      <input
+        class="inputFieldLogin"
+        type="text"
+        placeholder="Wachtwoord"
+        required
+      />
+    </div>
+  </body>
+</template>
+
+<style>
+.loginContainer {
+  background-color: aqua;
+  height: 500px;
+  width: 30%;
+  display: flex;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.inputFieldLogin {
+  height: 30px;
+  width: 250px;
+}
+
+body {
+  background-color: bisque;
+  position: relative;
+}
+</style>
