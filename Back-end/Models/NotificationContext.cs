c60cff6f9569dfd3779d8ead5ea3d8e757d91404
Serena Kenter <serena.kenter@4-rest.nl>
2023-09-12 10:58:54 +0200

    data in de db file

--- Back-end/Models/NotificationContext.cs
+++ Back-end/Models/NotificationContext.cs
@@ -8,10 +8,10 @@ namespace Back_end.Models
 {
     public class NotificationContext : DbContext
     {
-        public NotificationContext(DbContextOptions<NotificationContext> options) : base(options)
-        {
+        // public NotificationContext(DbContextOptions<NotificationContext> options) : base(options)
+        // {
 
-        }
+        // }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Customer> Customers { get; set; }
