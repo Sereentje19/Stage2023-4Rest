913af074cdbc980102ad40ef9453bd9e16060e76
Serena Kenter <serena.kenter@4-rest.nl>
2024-01-11 13:25:25 +0100

    dew

--- Front-end/src/views/ChangePassword.vue
+++ Front-end/src/views/ChangePassword.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="login-body">
-        <img id="logo-login" src="../assets/pictures/logo.png" alt="does not work" />
+        <img id="logo-login" src="/pictures/logo.png" alt="does not work" />
         <div id="login-container">
             <div id="background-circel">
                 <div id="alignment-body">
