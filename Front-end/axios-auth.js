2f5b837394b291fc0f042d7a6a3b1e172f412874
Serena Kenter <serena.kenter@4-rest.nl>
2024-01-12 09:28:38 +0100

    Update axios-auth.js

--- Front-end/axios-auth.js
+++ Front-end/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5050/',
+    baseURL: 'https://document-trace-manager.azurewebsites.net/',
     withCredentials: true, 
 });
 
