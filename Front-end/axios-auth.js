c8799aa567494f73d6144218a447fcbb44937e94
Serena Kenter <serena.kenter@4-rest.nl>
2024-01-19 10:20:11 +0100

    ssw

--- Front-end/axios-auth.js
+++ Front-end/axios-auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://document-trace-manager.azurewebsites.net/',
-    withCredentials: false, 
+    withCredentials: true, 
 });
 
 export default instance;
